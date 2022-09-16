82c8a99927427e450666bc782d58817b30be17ea
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-17 02:49:00 +0300

    Добавлено ДЗ №1 к семинару № 1

---
+++ CSharpIntroSeminar/HomeWorkNumber1/EXAMPLE№1-1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число a   :");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b   :");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) 
+{
+    Console.WriteLine("Максимальное число max = "+ a );
+    Console.WriteLine("Минимальное число min = "+ b );
+}    
+else 
+{
+Console.WriteLine("Максимальное число max = "+ b );
+Console.WriteLine("Минимальное число min = "+ a );
+}
