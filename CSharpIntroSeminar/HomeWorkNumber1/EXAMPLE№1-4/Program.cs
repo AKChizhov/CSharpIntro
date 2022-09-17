300a1784e4a8a7ed5fc8001886e43d75e6fc0e6f
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-17 04:15:53 +0300

    Добавлено ДЗ № 4 к семинару №1

---
+++ CSharpIntroSeminar/HomeWorkNumber1/EXAMPLE№1-4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число n:_");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while  (i <= n)  
+    {
+    if (i % 2 == 0) 
+        {
+        Console.Write (i);
+        }    
+    else
+        {
+        Console.Write (" ");
+        }
+     i++;
+    }
+
+
