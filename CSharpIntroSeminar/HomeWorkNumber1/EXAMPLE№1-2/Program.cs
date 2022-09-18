f8bb127e7d1385d6badeb46b8b7b6190210c3b9b
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-18 22:28:31 +0300

    Добавлено ДЗ №2 к семинару №1

---
+++ CSharpIntroSeminar/HomeWorkNumber1/EXAMPLE№1-2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число a:_");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b:_");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c:_");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Максимальное число из введенных :_");
+if (a >b && a > c ) Console.WriteLine (a);
+else if (b >a && b > c ) Console.WriteLine (b);
+else Console.WriteLine (c);
