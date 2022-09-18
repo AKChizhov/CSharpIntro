fe9c9102dd890de006457475e2f894aa728c0bd5
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-18 23:29:36 +0300

    Добавлено ДЗ №3 к семинару №1

---
+++ CSharpIntroSeminar/HomeWorkNumber1/EXAMPLE№1-3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 ==0) Console.WriteLine ("Введенное число "+n+" - четное");
+else Console.WriteLine ("Введенное число "+n+" - нечетное");
