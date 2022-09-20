8d28f8bad49dc78c77fe2bdba3c43a8ee52e9a6c
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-20 13:47:54 +0300

    Проводились работы

---
+++ CSharpIntroSeminar/HomeWorkNumber2/EXAMPLE№2-1/Program.cs
@@ -0,0 +1,21 @@
+﻿int Input()
+{
+Console.Clear();
+Console.WriteLine("Ведите число от 100 по 999");
+int enter = Convert.ToInt32(Console.ReadLine());
+return enter;
+}
+
+int n = Input ();
+
+ if (n >=100 && n<=999)
+{
+int n1 = (n%100 - n% 10)/10;
+Console.WriteLine(n1);
+}
+else 
+{
+ int n11 = Input();  
+ Console.WriteLine(n11);
+}
+
---
+++ CSharpIntroSeminar/HomeWorkNumber2/EXAMPLE№2-2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Task t = new Task(new Action(() => {
+
+while(true)
+{
+   Console.Beep();
+   Console.WriteLine("Press any key to stop it...");
+   Thread.Sleep(500);
+}
+}));
+t.Start();
+Console.WriteLine("Hello!!!");
+Console.ReadKey();
