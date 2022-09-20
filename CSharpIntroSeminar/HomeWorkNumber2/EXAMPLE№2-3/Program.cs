8f72bbcc196015852027d7a668366b661d976ff7
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-21 01:39:36 +0300

    Добавлено ДЗ №3 к семинару №2

---
+++ CSharpIntroSeminar/HomeWorkNumber2/EXAMPLE№2-3/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.WriteLine("Здравствуй пользователь ! ");
+int Input()
+{
+    Console.Write("Введи число от 1 по 7 :   ");//Обработка вводимого 
+    int enter;
+    Int32.TryParse(Console.ReadLine(), out enter);
+    return enter;
+}
+
+while (true)
+{
+    int n = Input();
+
+    if (n >= 1 && n <= 7)//Проверка вводимого
+    {
+        if (n == 6 | n == 7)
+        {
+            Console.WriteLine(n + "     Сегодня выходной  ");
+        }
+        else
+        {
+            Console.WriteLine(n + "     Сегодня работаем   ");
+        }
+        break;
+    }
+    Console.Beep();//Звуковой сигнал при неправильном вводе
+}
