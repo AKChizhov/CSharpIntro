3e1253a63230ba1b323f7a03fea777708ee1b6a8
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-20 23:53:11 +0300

    ДЗ №4 к семинару №2. Сделано форматирование

--- CSharpIntroSeminar/HomeWorkNumber2/EXAMPLE№2-4/Program.cs
+++ CSharpIntroSeminar/HomeWorkNumber2/EXAMPLE№2-4/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Здравствуй, пользователь !");
 
 int Input()
 {
-    Console.Write("Введи число от 2 по 20  "+'\n'+" - размерность массива случайных чисел : ");//Метод для обработки введенного
+    Console.Write("Введи число от 2 по 20  " + '\n' + " - размерность массива случайных чисел : ");//Метод для обработки введенного
     int enter;
     Int32.TryParse(Console.ReadLine(), out enter);
     return enter;
@@ -17,16 +17,16 @@ while (true)
     {
         int[] array = new int[n];
         int i = 0;
-        float summa=0;
+        float summa = 0;
         while (i < n)
         {
             array[i] = new Random().Next(1, 21);//Заполняем массив и вычисляем сумму всех его членов
-            summa=summa + array[i];
-            Console.Write(" "+array[i]);
+            summa = summa + array[i];
+            Console.Write(" " + array[i]);
             i++;
         }
-        Console.Write('\n'+"Среднее арифметическое чисел выуказанного массива :"+"{0: 0.00}",summa/n);
-        Console.Write('\n'+" ");
+        Console.Write('\n' + "Среднее арифметическое чисел выуказанного массива :" + "{0: 0.00}", summa / n);
+        Console.Write('\n' + " ");
         break;
     }
     Console.Beep();//Звуковой сигнал при неправильном вводе
