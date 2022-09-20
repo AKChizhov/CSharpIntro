4687461a2e07301910308c4a4648b68f7c92923a
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-20 23:08:12 +0300

    Доработано исключение неправильного ввода

--- CSharpIntroSeminar/HomeWorkNumber2/EXAMPLE№2-1/Program.cs
+++ CSharpIntroSeminar/HomeWorkNumber2/EXAMPLE№2-1/Program.cs
@@ -1,21 +1,22 @@
-﻿int Input()
+﻿Console.Clear();
+Console.WriteLine("Здравствуй пользователь ! ");
+int Input()
 {
-Console.Clear();
-Console.WriteLine("Ведите число от 100 по 999");
-int enter = Convert.ToInt32(Console.ReadLine());
-return enter;
+    Console.WriteLine("Ведите число от 100 по 999");
+    int enter; // = Convert.ToInt32(Console.ReadLine());
+    Int32.TryParse(Console.ReadLine(), out enter);
+    return enter;
 }
 
-int n = Input ();
+while (true)
+{
+    int n = Input();
 
- if (n >=100 && n<=999)
-{
-int n1 = (n%100 - n% 10)/10;
-Console.WriteLine(n1);
-}
-else 
-{
- int n11 = Input();  
- Console.WriteLine(n11);
+    if (n >= 100 && n <= 999)
+    {
+        int n1 = (n % 100 - n % 10) / 10;
+        Console.WriteLine(n1);
+        break;
+    }
 }
 
