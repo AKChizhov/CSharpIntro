ef927066c5c326f78c5aa0f6683b9e6dda0d69ce
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-20 22:54:39 +0300

    Добавлено ДЗ №4 к семинару №2

---
+++ CSharpIntroSeminar/HomeWorkNumber2/EXAMPLE№2-4/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine("Здравствуй, пользователь !");
+
+int Input()
+{
+    Console.Write("Введи число от 2 по 20  "+'\n'+" - размерность массива случайных чисел : ");//Метод для обработки введенного
+    int enter;
+    Int32.TryParse(Console.ReadLine(), out enter);
+    return enter;
+}
+
+while (true)
+{
+    int n = Input();
+
+    if (n >= 2 && n <= 21) //Проверка введенного
+    {
+        int[] array = new int[n];
+        int i = 0;
+        float summa=0;
+        while (i < n)
+        {
+            array[i] = new Random().Next(1, 21);//Заполняем массив и вычисляем сумму всех его членов
+            summa=summa + array[i];
+            Console.Write(" "+array[i]);
+            i++;
+        }
+        Console.Write('\n'+"Среднее арифметическое чисел выуказанного массива :"+"{0: 0.00}",summa/n);
+        Console.Write('\n'+" ");
+        break;
+    }
+    Console.Beep();//Звуковой сигнал при неправильном вводе
+}
+
