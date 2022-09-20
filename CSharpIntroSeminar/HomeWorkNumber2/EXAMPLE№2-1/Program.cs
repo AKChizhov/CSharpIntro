57217e09238dd5f3853e1bdbd7d9368eb0e7a957
AKChizhov <ChizhovAK2007@yandex.ru>
2022-09-20 23:48:24 +0300

    Добавлено ДЗ №1 к семинару №2

--- CSharpIntroSeminar/HomeWorkNumber2/EXAMPLE№2-1/Program.cs
+++ CSharpIntroSeminar/HomeWorkNumber2/EXAMPLE№2-1/Program.cs
@@ -2,8 +2,8 @@
 Console.WriteLine("Здравствуй пользователь ! ");
 int Input()
 {
-    Console.WriteLine("Ведите число от 100 по 999");
-    int enter; // = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введи число от 100 по 999 :   ");//Обработка вводимого 
+    int enter;
     Int32.TryParse(Console.ReadLine(), out enter);
     return enter;
 }
@@ -12,11 +12,12 @@ while (true)
 {
     int n = Input();
 
-    if (n >= 100 && n <= 999)
+    if (n >= 100 && n <= 999)//Проверка вводимого
     {
         int n1 = (n % 100 - n % 10) / 10;
-        Console.WriteLine(n1);
+        Console.WriteLine("Вторая цифра введенного числа :  " + n1);
         break;
     }
+    Console.Beep();//Звуковой сигнал при неправильном вводе
 }
 
