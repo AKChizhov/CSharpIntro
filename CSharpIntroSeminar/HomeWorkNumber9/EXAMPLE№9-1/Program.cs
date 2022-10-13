5b3ddafdc96a0d5f3c6c8f4fbb598937e8e155d5
AKChizhov <ChizhovAK2007@yandex.ru>
2022-10-13 17:47:06 +0300

    Работаем

---
+++ CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int[] array = new int[9];
+HashSet<int> set = new HashSet<int>();
+int i = 0;
+while (set.Count < 9)
+{
+     set.Add(new Random().Next(0,10));
+    
+    
+}
+
+
+/*var myEnum = array.GetEnumerator() ;
+while(myEnum.MoveNext())
+{
+  Console.WriteLine(myEnum.Current);
+}*/
+
+foreach(int c in set)
+{
+  Console.Write("  "+c); 
+ array[i] = c;
+    i++;
+}
+Console.WriteLine();
+foreach(int c in array)
+{
+  Console.Write("  "+c); 
+}
