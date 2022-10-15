1d528ae722ec88e846f7c165c120654276089033
AKChizhov <ChizhovAK2007@yandex.ru>
2022-10-16 00:19:24 +0300

    Добавлено преобразование массива в массив без цифр

--- CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-VERIFICATION_WORK/Program.cs
+++ CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-VERIFICATION_WORK/Program.cs
@@ -12,34 +12,65 @@ string[] CreateWordsArray(int size)
     return new string[size];
 }
 
-string[] FillWordsArray(string[] array)
+string[] FillWordsArray(string[] array,int min, int max)
 {
     int wordsNumer = array.Length;
-    Console.WriteLine(wordsNumer);
     for (int i = 0; i < wordsNumer; i++)
     {
-        int wordsSize = new Random().Next(3, 8);
+        int wordsSize = new Random().Next(min, max);
         array[i] = "";
         for (int j = 0; j < wordsSize; j++)
         {
-            int temp =new Random().Next(48,111);
+            int temp = new Random().Next(48, 111);
             array[i] = string.Concat(array[i] + Convert.ToChar(temp));
         }
     }
     return array;
 }
- 
+
 void PrintArray(string[] array)
 {
-    int size=array.Length;
+    int size = array.Length;
     Console.WriteLine();
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        Console.Write("   "+array[i]);
+        Console.Write("   " + array[i]);
     }
-} 
+}
+
+string[] ModifArray(string[] array)
+{
+    string[] number = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    int index = 0;
+    int size = array.GetLength(0);
+    for (int i = 0; i < size; i++)
+    {
+        foreach (char aa in array[i])
+        {
+            for (int j = 0; j <= 9; j++)
+            {
+                if (aa == Convert.ToChar(number[j]))
+                {
+                    array[i] = array[i].Replace(number[j], "");
+                }
+            }
+            index++;
+        }
+    }
+    return array;
+}
+
 
 int wordsNumber = CreateNumber(5, 16);
+Console.WriteLine("\n\tСоздан массив из "+wordsNumber+" строк (возможно с 5 по 15), в каждой из которых возможно с 3 по 7 символов"+
+"\n\tСимволы задаются кодами клавиш клавиатуры с 48 по 110");
 string[] ourWordsArray = CreateWordsArray(wordsNumber);
-ourWordsArray=FillWordsArray(ourWordsArray);
+Console.ForegroundColor = ConsoleColor.Red;
+ourWordsArray = FillWordsArray(ourWordsArray,3,8);
 PrintArray(ourWordsArray);
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("\n\n\tИсходный массив преобразован в массив, не содержащий цифр");
+ourWordsArray = ModifArray(ourWordsArray);
+Console.ForegroundColor = ConsoleColor.Green;
+PrintArray(ourWordsArray);
+Console.WriteLine();
