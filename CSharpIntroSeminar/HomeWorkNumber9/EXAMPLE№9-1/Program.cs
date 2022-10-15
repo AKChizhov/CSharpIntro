9bc116c5e342974d44a3e33811e06001cda6f9c4
AKChizhov <ChizhovAK2007@yandex.ru>
2022-10-15 21:05:25 +0300

    Еще работаем

--- CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-1/Program.cs
+++ CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-1/Program.cs
@@ -1,20 +1,10 @@
-﻿
-int[] array = new int[9];
+﻿int[] array = new int[9];
 HashSet<int> set = new HashSet<int>();
 int i = 0;
 while (set.Count < 9)
 {
-     set.Add(new Random().Next(0,10));
-    
-    
+     set.Add(new Random().Next(0,10));     
 }
-
-
-/*var myEnum = array.GetEnumerator() ;
-while(myEnum.MoveNext())
-{
-  Console.WriteLine(myEnum.Current);
-}*/
 
 foreach(int c in set)
 {
@@ -25,5 +15,38 @@ foreach(int c in set)
 Console.WriteLine();
 foreach(int c in array)
 {
-  Console.Write("  "+c); 
+  Console.Write("  "+c);
 }
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write ("\n\tЧто");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write ("-то\n");
+int[] ourArray={65,76,66,56} ;
+foreach(int d in ourArray)
+{
+  Console.Write(Convert.ToChar(d));
+}
+
+string[] number ={"0","1","2","3","4","5","6","7","8","9"};
+string data= "";
+for(int j=48;j<=110;j++) 
+{
+data = string.Concat(data +Convert.ToChar( j));
+}
+Console.WriteLine("\n\t"+data);
+Console.WriteLine("\t"+data.Length);
+int index=0;
+foreach(char aa in data)
+{
+  for(int zzz=0;zzz<=9;zzz++)
+  {
+  if(aa==Convert.ToChar(number[zzz]))
+  {
+  data=data.Replace(number[zzz],"");
+  }
+  }
+  index++;
+}
+Console.WriteLine("\t"+index);
+Console.WriteLine("\t"+data);
+
