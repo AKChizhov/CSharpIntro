0dd0feeccc9de32f837d6222ccefd6ab50ac0c7a
AKChizhov <ChizhovAK2007@yandex.ru>
2022-10-15 23:03:31 +0300

    Заполнение массива строками, у которых длины и элементы получены псевдослучайно

--- CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-VERIFICATION_WORK/Program.cs
+++ CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-VERIFICATION_WORK/Program.cs
@@ -12,5 +12,25 @@ string[] CreateWordsArray(int size)
     return new string[size];
 }
 
+string[] FillWordsArray(string[] array)
+{
+    int wordsNumer = array.Length;
+    Console.WriteLine(wordsNumer);
+    for (int i = 0; i < wordsNumer; i++)
+    {
+        int wordsSize = new Random().Next(3, 8);
+        array[i] = "";
+        for (int j = 0; j < wordsSize; j++)
+        {
+            int temp =new Random().Next(48,111);
+            array[i] = string.Concat(array[i] + Convert.ToChar(temp));
+        }
+        Console.Write("\t" + array[i]);
+    }
+    return array;
+}
+
+
 int wordsNumber = CreateNumber(5, 16);
 string[] ourWordsArray = CreateWordsArray(wordsNumber);
+ourWordsArray=FillWordsArray(ourWordsArray);
