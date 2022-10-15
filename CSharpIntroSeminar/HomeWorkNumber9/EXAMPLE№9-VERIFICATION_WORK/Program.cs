4ba16026370273b97490d94c3e817163cf2f20a1
AKChizhov <ChizhovAK2007@yandex.ru>
2022-10-16 00:36:09 +0300

    Добавлено описание программы

--- CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-VERIFICATION_WORK/Program.cs
+++ CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-VERIFICATION_WORK/Program.cs
@@ -1,18 +1,25 @@
 ﻿/*
-
+Задача: Написать программу, которая из сформированного массива строк содержащих символы и числа создать массив из строк,
+в который попадут только символы, не являющиеця цифрами. Входные данные можно получить путём ввода с клавиатуры,
+но лучше сделать автогенерацию данных. При решении не использовать "читерство".
+Количество строк в массиве определяется псевдослучайно в интервале с 5 по 15. Количество символов в строке - псевдослучайно
+в интервале с 3 по 3. Каждый символ - псевдослучайно в интервале с 48 по 110 (коды клавиш клавиатуры). 
+Где :
+ --- wordsNumber - число строк в массиве;
+ --- ourWordsArray -искомый массив ;
 */
 
-int CreateNumber(int min, int max)
+int CreateNumber(int min, int max)//Определение количества строк в массиве
 {
     return new Random().Next(min, max);
 }
 
-string[] CreateWordsArray(int size)
+string[] CreateWordsArray(int size)//Создание массива определенной размерности;
 {
     return new string[size];
 }
 
-string[] FillWordsArray(string[] array,int min, int max)
+string[] FillWordsArray(string[] array,int min, int max)//Заполнение массива символами, в том числе и цифрами;
 {
     int wordsNumer = array.Length;
     for (int i = 0; i < wordsNumer; i++)
@@ -28,7 +35,7 @@ string[] FillWordsArray(string[] array,int min, int max)
     return array;
 }
 
-void PrintArray(string[] array)
+void PrintArray(string[] array)//вывод массива
 {
     int size = array.Length;
     Console.WriteLine();
@@ -38,7 +45,7 @@ void PrintArray(string[] array)
     }
 }
 
-string[] ModifArray(string[] array)
+string[] ModifArray(string[] array)//Преобразование массива в массив не содержащий цифры
 {
     string[] number = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
     int index = 0;
@@ -61,12 +68,12 @@ string[] ModifArray(string[] array)
 }
 
 
-int wordsNumber = CreateNumber(5, 16);
+int wordsNumber = CreateNumber(5, 16);//Для наглядности на экране
 Console.WriteLine("\n\tСоздан массив из "+wordsNumber+" строк (возможно с 5 по 15), в каждой из которых возможно с 3 по 7 символов"+
 "\n\tСимволы задаются кодами клавиш клавиатуры с 48 по 110");
 string[] ourWordsArray = CreateWordsArray(wordsNumber);
 Console.ForegroundColor = ConsoleColor.Red;
-ourWordsArray = FillWordsArray(ourWordsArray,3,8);
+ourWordsArray = FillWordsArray(ourWordsArray,3,8);//Для наглядности на экране
 PrintArray(ourWordsArray);
 Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine("\n\n\tИсходный массив преобразован в массив, не содержащий цифр");
