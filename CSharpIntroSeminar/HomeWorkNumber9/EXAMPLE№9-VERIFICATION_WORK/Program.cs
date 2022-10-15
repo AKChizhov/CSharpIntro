5b073ba067a11f566b5c0e615a265c169134e7e1
AKChizhov <ChizhovAK2007@yandex.ru>
2022-10-15 22:00:06 +0300

    Создан массив строк

--- CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-VERIFICATION_WORK/Program.cs
+++ CSharpIntroSeminar/HomeWorkNumber9/EXAMPLE№9-VERIFICATION_WORK/Program.cs
@@ -2,13 +2,15 @@
 
 */
 
-int CreateNumber(int min,int max)
+int CreateNumber(int min, int max)
 {
-    return new Random().Next(min,max);
+    return new Random().Next(min, max);
 }
 
+string[] CreateWordsArray(int size)
+{
+    return new string[size];
+}
 
-
-
-
-int wordsNumber = CreateNumber(5,16);
+int wordsNumber = CreateNumber(5, 16);
+string[] ourWordsArray = CreateWordsArray(wordsNumber);
